796220d0bbb8a3661d4175eff10f283f2ea66a63
Mehrdad Shahabi <mmsh@mehrdads-mbp.mynetworksettings.com>
2024-06-16 21:03:11 -0400

    OPEN AI Clinet is added

--- Main.py
+++ Main.py
@@ -3,13 +3,21 @@ from langchain.agents import AgentExecutor
 from langchain_experimental.tools import PythonREPLTool
 
 
-# import python tools
-tools = [PythonREPLTool()]
 
 
 from langchain.agents import create_openai_functions_agent
 from langchain_openai import ChatOpenAI
 
+from dotenv import load_dotenv, find_dotenv
+import os
+from pathlib import Path
+
+_ = load_dotenv(find_dotenv())
+Key = os.getenv("OPENAI_API_KEY")
+
+
+# import python tools
+tools = [PythonREPLTool()]
 instructions = """YYou want to sell a certain number  
   of items in order to maximize your profit. Market research tells you that if you set the price at $1.50, 
   you will be able to sell 5000 items, and for every 10 cents you lower the price below $1.50 you will be able to sell another 1000 items. 
@@ -17,3 +25,10 @@ instructions = """YYou want to sell a certain number
 """
 base_prompt = hub.pull("langchain-ai/openai-functions-template")
 prompt = base_prompt.partial(instructions=instructions)
+
+agent = create_openai_functions_agent(ChatOpenAI(temperature=0), tools, prompt)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+agent_executor.invoke({"input": "What is the 10th fibonacci number?"})
+
