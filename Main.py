e96a222822cf41e5a460ddb216ff2f1336f3a6a0
Mehrdad Shahabi <mmsh@mehrdads-mbp.mynetworksettings.com>
2024-06-17 23:04:03 -0400

    pyomo installed

--- Main.py
+++ Main.py
@@ -18,17 +18,16 @@ Key = os.getenv("OPENAI_API_KEY")
 
 # import python tools
 tools = [PythonREPLTool()]
-instructions = """YYou want to sell a certain number  
-  of items in order to maximize your profit. Market research tells you that if you set the price at $1.50, 
-  you will be able to sell 5000 items, and for every 10 cents you lower the price below $1.50 you will be able to sell another 1000 items. 
-  Suppose that your fixed costs ( “start-up costs” ) total $2000, and the per item cost of production ( “marginal cost” ) is $0.50.
-"""
-base_prompt = hub.pull("langchain-ai/openai-functions-template")
-prompt = base_prompt.partial(instructions=instructions)
 
-agent = create_openai_functions_agent(ChatOpenAI(temperature=0), tools, prompt)
+instructions = """use git clone https://github.com/Pyomo/pyomo"""
+
+base_prompt = hub.pull("langchain-ai/openai-functions-template")
+
+prompt = base_prompt.partial(instructions=instructions)
+print(prompt)
+agent = create_openai_functions_agent(ChatOpenAI(temperature=0.5), tools, prompt)
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-agent_executor.invoke({"input": "What is the 10th fibonacci number?"})
+agent_executor.invoke({"input": "Find the price to set per item and the number of items sold in order to maximize profit, and also determine the maximum profit you can get?"})
 
